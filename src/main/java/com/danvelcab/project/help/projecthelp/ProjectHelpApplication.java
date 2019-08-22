81c04f7c3018b7e6b457acf12bcdc12c3906ea71
Daniel Velázquez <danvelcab@gmail.com>
2019-08-22 16:44:09 +0200

    . proyecto inicializado

---
+++ src/main/java/com/danvelcab/project/help/projecthelp/ProjectHelpApplication.java
@@ -0,0 +1,13 @@
+package com.danvelcab.project.help.projecthelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHelpApplication.class, args);
+	}
+
+}
